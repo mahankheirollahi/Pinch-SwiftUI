c31e69ad8047d54dd095e7c597490f57d351ca29
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-07 16:35:57 +0300

    Add drag gesture

--- Pinch-SwiftUI/Screens/ContentView.swift
+++ Pinch-SwiftUI/Screens/ContentView.swift
@@ -11,6 +11,15 @@ struct ContentView: View {
     //MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    //MARK: - FUNCTION
+    func resetImageState(){
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     var body: some View {
         NavigationView{
@@ -23,20 +32,35 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 12,x: 2,y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x:imageOffset.width,y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
                 //MARK: - TAP GESTURE
                     .onTapGesture(count:2,perform: {
-                    if imageScale == 1{
-                        withAnimation(.spring()){
-                            imageScale = 5
+                        if imageScale == 1{
+                            withAnimation(.spring()){
+                                imageScale = 5
+                            }
+                        }else{
+                           resetImageState()
                         }
-                    }else{
-                        withAnimation(.spring()){
-                            imageScale = 1
-                        }
-                    }
-                })
+                    })
+                //MARK: - DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged{value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded{_ in
+                                
+                                if imageScale <= 1{
+                                    resetImageState()
+                                }
+                            }
+                    )
+                
             }//: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -44,13 +68,13 @@ struct ContentView: View {
                 withAnimation(.linear(duration: 1)){
                     isAnimating = true
                 }
-               
+                
             })
         } //: NAVIGATION VIEW
         .navigationViewStyle(.stack)
         
     }
-      
+    
 }
 
 #Preview {
