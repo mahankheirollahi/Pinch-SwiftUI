17a82fe8f40288346a61a62c861e70f5808e440b
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-07 15:43:01 +0300

    Initial Commit

---
+++ Pinch-SwiftUI/Pinch_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_SwiftUIApp.swift
+//  Pinch-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
