9901e3a00c67e293c63aba103f6a91e3b9230dda
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-07 16:16:13 +0300

    Add cover image and implement double tap scale gesture

--- Pinch-SwiftUI/Screens/ContentView.swift
+++ Pinch-SwiftUI/Screens/ContentView.swift
@@ -8,15 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView{
+            ZStack{
+                //MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 12,x: 2,y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                //MARK: - TAP GESTURE
+                    .onTapGesture(count:2,perform: {
+                    if imageScale == 1{
+                        withAnimation(.spring()){
+                            imageScale = 5
+                        }
+                    }else{
+                        withAnimation(.spring()){
+                            imageScale = 1
+                        }
+                    }
+                })
+            }//: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+               
+            })
+        } //: NAVIGATION VIEW
+        .navigationViewStyle(.stack)
+        
     }
+      
 }
 
 #Preview {
