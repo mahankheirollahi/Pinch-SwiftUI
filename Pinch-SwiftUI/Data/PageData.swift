76b5a791aadc76f7aaec015d3d08f9d4ff0cb7d8
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-22 16:08:55 +0300

    Add constant page's data

---
+++ Pinch-SwiftUI/Data/PageData.swift
@@ -0,0 +1,14 @@
+//
+//  PageData.swift
+//  Pinch-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 7/22/24.
+//
+
+import Foundation
+
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
